24c8d731732446fa6bca20efe6d52cae9d63c6cd
Maelle Chenaux <maellechenaux@MacBook-Pro-de-Maelle-3.local>
2019-01-09 15:13:28 +0100

    change ip in webpage

--- webpage/js/index.js
+++ webpage/js/index.js
@@ -1,4 +1,6 @@
-var PAGE_IP_ADDRESS = "172.20.10.12:3000";
+//var PAGE_IP_ADDRESS = "172.20.10.12:3000";
+var PAGE_IP_ADDRESS = "10.192.162.21:3000";
+
 // var PAGE_IP_ADDRESS = "localhost:3000";
 
 $("#sender").click(function() {
