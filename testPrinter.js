95f0c3fdeb6378eb0d16317d804c23d2b46d26a7
Maelle Chenaux <maellechenaux@MacBook-Pro-de-Maelle-3.local>
2019-01-11 12:04:36 +0100

    y0

--- testPrinter.js
+++ testPrinter.js
@@ -1,5 +1,5 @@
 var SerialPort = require('serialport'),
-    serialPort = new SerialPort('/dev/ttyUSB0', {
+    serialPort = new SerialPort('/dev/mmcblk0p2', {
         baudRate: 19200
     }),
     Printer = require('thermalprinter');
