b14e95e70062f2cd44b586b1c109e56b016cb028
Maelle Chenaux <maellechenaux@MacBook-Pro-de-Maelle-3.local>
2019-01-13 21:42:52 +0100

    date print

--- testPrinter.js
+++ testPrinter.js
@@ -1,3 +1,9 @@
+var dt = new Date();
+var date = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear());
+var time =(("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
+
+
+
 var SerialPort = require('serialport'),
     serialPort = new SerialPort('/dev/ttyS0', {
         baudRate: 19200
@@ -11,15 +17,19 @@ serialPort.on('open',function() {
     printer.on('ready', function() {
       console.log("printing");
         printer
-        .horizontalLine(10)
+        .center()
+        .horizontalLine(16)
         .bold(true)
-        .underline()
         .printLine('GOOD MORNING')
         .bold(false)
         .inverse(true)
         .big(true)
-        .center()
         .printLine('Le quotidien')
+        .horizontalLine(16)
+        .big(false)
+        .inverse(false)
+        .printLine(date)
+
             .print(function() {
                 console.log('done');
                 process.exit();
